7734b200d1622df17a5589d62f8d01d27298497c
An00bRektn <An00bRektn@users.noreply.github.com>
2022-08-19 14:52:09 -0500

    Created Windows startup script

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Ignition Key
 > March 19, 2022 | An00bRektn
 
-This repository contains an automated setup script various VMs so I can get my preferred tools set up quickly. Currently untested, but too lazy to do that right now.
+This repository contains an automated setup script various VMs so I can get my preferred tools set up quickly.
 
 Inspired by John Hammond's [ignition-key](https://github.com/JohnHammond/ignition_key/blob/master/ignition_key.sh) script, and Dewalt's [pimp-my-kali](https://github.com/Dewalt-arch/pimpmykali) script.
 
 ### TODO
-- [ ] Windows Dev machine setup using choco + pwsh
-- [ ] Adding any additional tools/languages needed in the \*nix scripts
+- [x] Windows Dev machine setup using choco + pwsh
+    - [ ] Test the Windows dev install script
+- [ ] Test Ubuntu script
 - [ ] More wallpapers and/or terminal customization?
 
 **NOTE**: Author assumes zero liability for any data loss or misuse (or use) of the script. Can be used on a bare metal machines, but that's on you.
---
+++ win-ignition-key.ps1
@@ -0,0 +1,47 @@
+<#
+    Windows Development VM Install Script
+    To run:
+        1. Open a new terminal window as Administrator
+        2. Allow Script execution by running "Set-ExecutionPolicy Unrestricted"
+        3. Execute with ".\win-ignition-key.ps1"
+#>
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
+if (-Not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "[!] Please run this script as administrator`n" -ForegroundColor Red
+    Read-Host  "Press any key to continue"
+    exit
+}
+
+. { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression
+Get-Boxstarter -Force
+
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/An00bRektn/ignition-key/main/boxstarter/windev.txt
+
+# For everything that isn't in chocolatey - too lazy to make a custom
+# package like flare or commando
+<#
+- pestudio
+-  
+#>
+New-Item -Path C:\Tools -ItemType Directory
+
+Invoke-WebRequest -Uri 'https://www.winitor.com/tools/pestudio/current/pestudio.zip' -OutFile C:\Tools\pestudio.zip
+Invoke-WebRequest -Uri 'https://aka.ms/vs/17/release/vs_BuildTools.exe' -OutFile C:\Tools\vs_BuildTools.exe
+Invoke-WebRequest -Uri 'http://sandsprite.com/CodeStuff/scdbg.zip' -OutFile C:\Tools\scdbg.zip
+
+Expand-Archive C:\Tools\pestudio.zip -DestinationPath C:\Tools\pestudio
+Expand-Archive C:\Tools\scdbg.zip -DestinationPath C:\Tools\scdbg
+
+Invoke-Expression 'git clone https://github.com/DidierStevens/DidierStevensSuite.git C:\Desktop\DiderStevensSuite'
+
+# A bunch of things that I don't want to automate because they're very likely to break :/
+Write-Host "[+] Install Complete! Here's a checklist of things you might also want to do" -ForegroundColor Green
+Write-Host "  \\--> Set the wallpaper" -ForegroundColor Yellow
+Write-Host "  \\--> VS Build tool setup in C:\Tools" -ForegroundColor Yellow
+Write-Host "  \\--> Download BurpSuite? (https://portswigger.net/burp/releases/professional-community-2022-8-2)" -ForegroundColor Yellow
+Write-Host "  \\--> Install EricZimmerman tools (https://ericzimmerman.github.io/#!index.md)" -ForegroundColor Yellow
+Write-Host "  \\--> Create an exception for Defender so it doesn't choke and die (also probably grab ThreatCheck)" -ForegroundColor Yellow
+
+Read-Host "Press any key to continue..."
+exit
